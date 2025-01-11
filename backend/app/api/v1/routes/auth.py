6dc94fb3c756ef711035fe6bb2add3f11ae5661a
khoroshok-a <khoroshok.a@yandex.ru>
2025-01-11 18:11:25 +0300

    edit auth routes

--- backend/app/api/v1/routes/auth.py
+++ backend/app/api/v1/routes/auth.py
@@ -1,15 +1,17 @@
 from fastapi import APIRouter
 
+from schemes import LoginSchema, RegisterShema
+
 router = APIRouter(prefix='/auth')
 
 
 @router.post('/login')
-async def login():
+async def login(login_data: LoginSchema):
     pass
 
 
 @router.post('/register')
-async def register():
+async def register(register_data: RegisterShema):
     pass
 
 
