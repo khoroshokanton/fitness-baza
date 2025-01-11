5132fc9365bb9c09b5dc1fe2f23e9d2857a1379f
khoroshok-a <khoroshok.a@yandex.ru>
2025-01-11 23:50:18 +0300

    add lifespan fastapi

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,20 @@
 import uvicorn
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
 from core.config import settings
 from api import router_v1
+
+from core.database import database_manager
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+    await database_manager.dispose()
+
 
 app = FastAPI()
 app.include_router(router=router_v1, prefix=settings.api.prefix)
